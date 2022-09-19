6de3c549ebec8d836d337e95ad933de01bdd46a1
sujoy ghoshal <92462200+sujoyghoshal@users.noreply.github.com>
2022-09-19 11:15:13 +0530

    Add files via upload

---
+++ sujoy gfg potd/19.cpp
@@ -0,0 +1,28 @@
+stack<int>se;
+        vector<int>ans;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(se.empty())
+            {
+                ans.push_back(-1);
+            }
+            else if(se.top()<a[i])
+            {
+                ans.push_back(se.top());
+            }
+            else
+            {
+                while(!se.empty() and se.top()>=a[i])
+                se.pop();
+                
+                if(se.empty())
+                ans.push_back(-1);
+                else
+                ans.push_back(se.top());
+               
+            }
+             se.push(a[i]);
+        }
+        return ans;
+    
